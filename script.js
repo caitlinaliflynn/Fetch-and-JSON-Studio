3b355a16fe2e0650318c4c3a2b51b577459a9c1b
Ali <caitlinaliflynn@gmail.com>
2023-03-14 00:46:38 -0500

    finished fetch and json studio

--- script.js
+++ script.js
@@ -1,1 +1,65 @@
-// TODO: add code here
+window.addEventListener('load', function() {
+
+    const container = document.getElementById('container');
+    const count = document.getElementById('count');
+
+    // fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+    //     response.json().then(function(data) {
+    //         //console.log(data);
+
+    //         count.innerHTML = `There are ${data.length} astronauts!`;
+
+    //         data.sort(function(a, b) {
+    //             return a.hoursInSpace < b.hoursInSpace ? 1 : -1;
+    //         });
+
+    //         for (let i = 0; i < data.length; i++) {
+    //             let astronaut = data[i];
+    //             container.innerHTML += `
+    //             <div class="astronaut">
+    //             <div class="bio">
+    //             <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+    //             <ul>
+    //             <li>Hours in space: ${astronaut.hoursInSpace}</li>
+    //             <li class="${astronaut.active ? "green" : ""}">Active: ${astronaut.active}</li>
+    //             <li>Skills: ${astronaut.skills.join(', ')}</li>
+    //             </ul>
+    //             </div>
+    //             <img class="avatar" src="${astronaut.picture}">
+    //             </div>
+    //             `;
+    //         };
+
+    //     });
+    // });
+
+    async function fetchAndDisplayAstronauts() {
+        let response = await fetch("https://handlers.education.launchcode.org/static/astronauts.json");
+        let data = await response.json();
+        count.innerHTML = `There are ${data.length} astronauts!`;
+
+        data.sort(function(a, b) {
+            return a.hoursInSpace < b.hoursInSpace ? 1 : -1;
+        });
+
+        for (let i = 0; i < data.length; i++) {
+            let astronaut = data[i];
+            container.innerHTML += `
+            <div class="astronaut">
+            <div class="bio">
+            <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+            <ul>
+            <li>Hours in space: ${astronaut.hoursInSpace}</li>
+            <li class="${astronaut.active ? "green" : ""}">Active: ${astronaut.active}</li>
+            <li>Skills: ${astronaut.skills.join(', ')}</li>
+            </ul>
+            </div>
+            <img class="avatar" src="${astronaut.picture}">
+            </div>
+            `;
+        };
+
+    }
+    fetchAndDisplayAstronauts();
+
+});
